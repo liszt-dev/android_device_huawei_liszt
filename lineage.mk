ae179142d9aa053bc396b2daeefb480e7724e762
schwienernitzel <pfelix0803@gmail.com>
2024-06-20 22:45:28 +0000

    mozart: Inherit device shipping API level
    Signed-off-by: schwienernitzel <pfelix0803@gmail.com>

--- lineage.mk
+++ lineage.mk
@@ -17,6 +17,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/locales_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
