e75eb042116266cd4b01a32762556d28a90c1cbc
Huawei-Dev <surdu_petru@yahoo.com>
2024-06-21 08:34:17 +0000

    mozart: Import google media codecs too
    Signed-off-by: schwienernitzel <pfelix0803@gmail.com>

--- device.mk
+++ device.mk
@@ -57,6 +57,11 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/media/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
     $(DEVICE_PATH)/configs/media/media_profiles.xml:system/etc/media_profiles.xml
 
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
+
 # Thermal
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/thermal/thermald.xml:system/etc/thermald.xml \
