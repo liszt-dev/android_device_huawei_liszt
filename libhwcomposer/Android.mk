4cc0c543a43348946267ad76326210a41b2b320f
schwienernitzel <pfelix0803@gmail.com>
2024-12-05 09:44:22 +0000

    liszt: libhwcomposer: Revert to original implementation
    Signed-off-by: schwienernitzel <pfelix0803@gmail.com>

--- libhwcomposer/Android.mk
+++ libhwcomposer/Android.mk
@@ -1,6 +1,7 @@
 #
-# Meticulus hi3635 libhwcomposer
-# Copyright (c) 2017 Jonathan Dennis [Meticulus] - theonejohnnyd@gmail.com
+# Meticulus hi6250 libhwcomposer
+# Copyright (c) 2017 Jonathan Dennis [Meticulus]
+#                               theonejohnnyd@gmail.com
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,15 +16,18 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
 
 LOCAL_PATH := $(call my-dir)
 
+# HAL module implemenation stored in
+# hw/<OVERLAY_HARDWARE_MODULE_ID>.<ro.product.board>.so
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SHARED_LIBRARIES := libutils liblog libEGL
 LOCAL_SRC_FILES := hwcomposer.cpp
-LOCAL_MODULE := hwcomposer.hi3635
+LOCAL_MODULE := hwcomposer.hi6250
 LOCAL_MODULE_TAGS := optional
 
 ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
